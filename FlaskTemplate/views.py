85434e1255dedc830f9f480a66eb0af223c7adfa
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-01 19:50:40 +0200

    Debug (conn.string)

--- FlaskTemplate/views.py
+++ FlaskTemplate/views.py
@@ -2,8 +2,8 @@
 Routes and views for the flask application.
 """
 import os
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
+# from azure.keyvault.secrets import SecretClient
+# from azure.identity import DefaultAzureCredential
 
 from datetime import datetime
 from flask import render_template
@@ -33,18 +33,19 @@ def contact():
 @app.route('/about')
 def about():
     """Renders the about page."""
-    keyVaultName = os.environ['KEY_VAULT_NAME']
-    KVUri = f'https://{keyVaultName}.vault.azure.net'
+    # keyVaultName = os.environ['KEY_VAULT_NAME']
+    # KVUri = f'https://{keyVaultName}.vault.azure.net'
 
-    credential = DefaultAzureCredential()
-    client = SecretClient(vault_url=KVUri, credential=credential)
+    # credential = DefaultAzureCredential()
+    # client = SecretClient(vault_url=KVUri, credential=credential)
 
-    secretName = 'maksym-ganistrat-1'
-    retrieved_secret = client.get_secret(secretName)
+    # secretName = 'maksym-ganistrat-1'
+    # retrieved_secret = client.get_secret(secretName)
     return render_template(
         'about.html',
         title='About',
         year = datetime.now().year,
-        message = str(retrieved_secret.value),
+        message = 'Test message'
+        # message = str(retrieved_secret.value),
         message2 = os.environ['ConnectionString']
     )
