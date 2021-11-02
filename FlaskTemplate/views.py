506de0587c6072940ed18101d83d62ea423f8702
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-02 13:17:48 +0200

    SyntaxFix

--- FlaskTemplate/views.py
+++ FlaskTemplate/views.py
@@ -48,5 +48,5 @@ def about():
         message = 'Test message',
         # message = str(retrieved_secret.value),
         message2 = os.environ['ConnectionString'],
-        message3 = f'This page running on {os.environ['WEBSITE_SITE_NAME']} instance'
+        message3 = os.environ['WEBSITE_SITE_NAME']
     )
