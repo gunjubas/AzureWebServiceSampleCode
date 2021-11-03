818bfa207a0df7d28bfd2cb11fd6111599fab31e
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-03 13:24:14 +0200

    Create preinit.sh

---
+++ FlaskTemplate/preinit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt install -y install build-essential
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
+ACCEPT_EULA=Y apt-get install -y mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
