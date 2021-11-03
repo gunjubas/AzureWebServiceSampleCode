e1bd49662b06684ea0354d5045f215ce02a65591
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-03 18:49:31 +0200

    Debug preinit

--- FlaskTemplate/preinit.sh
+++ FlaskTemplate/preinit.sh
@@ -1,11 +1,10 @@
-!/bin/bash
-apt-get update
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
-ACCEPT_EULA=Y apt-get install -y mssql-tools
-apt-get install -y build-essential
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
-pip install pyodbc
+#!/bin/bash
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+sudo curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+sudo apt-get install -y build-essential
+sudo echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+sudo source ~/.bashrc
+sudo pip install pyodbc
