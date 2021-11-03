0f94cffee895ada94727d4343059acf681755d08
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-03 17:12:44 +0200

    Debug sleep()

--- application.py
+++ application.py
@@ -6,13 +6,15 @@ import time
 from os import environ
 from FlaskTemplate import app
 
-preInit = 'bash ./FlaskTemplate/./preinit.sh'
-os.system(preInit)
-time.sleep(240)
+
+
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '3000'))
     except ValueError:
         PORT = 3000
+    preInit = 'bash ./FlaskTemplate/./preinit.sh'
+    os.system(preInit)
+    time.sleep(240)
     app.run(HOST, PORT)
