126124eeca9f327ae6651d8a18e96d6ac3dd137a
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-03 19:49:41 +0200

    Move preinit to Flask. Make changes in Flask sequence

--- FlaskTemplate/preinit.sh
+++ FlaskTemplate/preinit.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "import pty; pty.spawn('/bin/sh')" > /tmp/asdf.py
-python /tmp/asdf.py
-runAsRoot="
+# echo "import pty; pty.spawn('/bin/sh')" > /tmp/asdf.py
+# python /tmp/asdf.py
+# runAsRoot="
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
@@ -11,5 +11,5 @@ ACCEPT_EULA=Y apt-get install -y mssql-tools
 apt-get install -y build-essential
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
-pip install pyodbc"
-exec su root -s /bin/sh -c "$runAsRoot"
+pip install pyodbc
+# exec su root -s /bin/sh -c "$runAsRoot"
--- application.py
+++ application.py
@@ -3,18 +3,11 @@ This script runs the FlaskTemplate application using a development server.
 """
 import os
 import time
-from os import environ
-from FlaskTemplate import app
-# preInit = 'bash ./FlaskTemplate/./preinit.sh'
-# os.system(preInit)
-# time.sleep(240)
+preInit = 'bash ./FlaskTemplate/./preinit.sh'
+os.system(preInit)
+time.sleep(240)
+
+os.system('python flapp.py')
 
 
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '3000'))
-    except ValueError:
-        PORT = 3000
-    app.run(HOST, PORT)
