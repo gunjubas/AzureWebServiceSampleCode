0f32c057eab39d32cd38089ee01b588bcdb517bd
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-04 17:09:05 +0200

    Implement query in separate script #7

--- FlaskTemplate/query.py
+++ FlaskTemplate/query.py
@@ -3,14 +3,14 @@ import os
 sqlResponse = []
 with pyodbc.connect(os.environ['ConnectionString']) as conn:
     with conn.cursor() as cursor:
-        cursor.execute("SELECT * FROM [SalesLT].[Customer] WHERE [CustomerID] < '21'")
+        cursor.execute("SELECT SELECT [CustomerID],[Title],[FirstName],[MiddleName],[LastName],[CompanyName],[EmailAddress],[Phone] FROM [SalesLT].[Customer] WHERE [CustomerID] < 21 FROM [SalesLT].[Customer] WHERE [CustomerID] < '21'")
         #cursor.execute("SELECT [Title], [FirstName], [MiddleName], [LastName], [CompanyName], [Phone] FROM [SalesLT].[Customer] WHERE [CustomerID] < 21")
         row = cursor.fetchone()
         while row is not None:
-            sqlResponse.append('<tr>')
+            sqlResponse.append('\<tr\>')
             for i in row:
-                sqlResponse.append(f'<td>{i}</td>')
-            sqlResponse.append('</tr>')
+                sqlResponse.append(f'\<td\>{i}\</td\>')
+            sqlResponse.append('\</tr\>')
             row = cursor.fetchone()
 
 print(*sqlResponse[:])
