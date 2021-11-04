858d7e9bfeb2643e6432d87a38b463dc2a231157
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-04 11:35:46 +0200

    Step 3: Enable preInit

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 """
 This script runs the FlaskTemplate application using a development server.
 """
-# import os
-# import time
-# preInit = 'bash ./FlaskTemplate/./preinit.sh'
-# os.system(preInit)
-# time.sleep(180)
+import os
+import time
+preInit = 'bash ./FlaskTemplate/./preinit.sh'
+
 
 # os.system('python flapp.py')
 from os import environ
@@ -16,4 +15,6 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '3000'))
     except ValueError:
         PORT = 3000
+    os.system(preInit)
+    time.sleep(180)
     app.run(HOST, PORT)
