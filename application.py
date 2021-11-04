42c14458377f2f41782672ed0de252a8231bf419
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-04 10:42:41 +0200

    Debug with remote debuggin

--- application.py
+++ application.py
@@ -1,13 +1,23 @@
 """
 This script runs the FlaskTemplate application using a development server.
 """
-import os
-import time
+# import os
+# import time
 # preInit = 'bash ./FlaskTemplate/./preinit.sh'
 # os.system(preInit)
 # time.sleep(180)
 
-os.system('python flapp.py')
+# os.system('python flapp.py')
 
 
 
+
+from os import environ
+from FlaskTemplate import app
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '3000'))
+    except ValueError:
+        PORT = 3000
+    app.run(HOST, PORT)
