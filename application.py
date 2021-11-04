f205559daba2cb65fa3309e2a4fee2088e76fb5f
Maksym Ganistrat <maksym_ganistrat@outlook.com>
2021-11-04 10:29:29 +0200

    Debug preinit

--- application.py
+++ application.py
@@ -3,9 +3,9 @@ This script runs the FlaskTemplate application using a development server.
 """
 import os
 import time
-preInit = 'bash ./FlaskTemplate/./preinit.sh'
-os.system(preInit)
-time.sleep(240)
+# preInit = 'bash ./FlaskTemplate/./preinit.sh'
+# os.system(preInit)
+# time.sleep(180)
 
 os.system('python flapp.py')
 
